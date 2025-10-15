4dca27c187cc634ee5abbba3cd75493502b6aca8
Rizqdwan <rizqdwandhuhakbar@gmail.com>
2025-10-16 02:04:16 +0700

    feat: list deck cards

--- deck.go
+++ deck.go
@@ -6,6 +6,22 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for eachSuit := range cardSuits {
+		for eachValue := range cardValues {
+			cards = append(cards, cardValues[eachValue] + " of " + cardSuits[eachSuit])
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print(){
 	for i, card := range d {
 		fmt.Println(i, card)
@@ -17,5 +33,5 @@ func (d deck) shuffle(){
 }
 
 func (d deck) saveToFile(){
-	
+
 }
