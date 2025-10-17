c2f2f9f9f4844501b2942122afd138c193b4df70
Rizqdwan <rizqdwandhuhakbar@gmail.com>
2025-10-18 00:59:13 +0700

    feat: save local file

--- deck.go
+++ deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 // Create a new type of 'deck'
 // which is a slice of strings
@@ -28,10 +32,30 @@ func (d deck) print(){
 	}
 }
 
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+
+}
+
 func (d deck) shuffle(){
 
 }
 
-func (d deck) saveToFile(){
 
+func (d deck) toString() string {
+	return strings.Join([]string(d), ", ")	
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck{
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ", ")
+	return deck(s)
 }
